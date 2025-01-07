356799cf085c6018d265d2f0abd9831fe4da12d5
saiful7778 <saiful.islam.rafi.88@gmail.com>
2025-01-08 00:32:51 +0600

    Add: add Data table

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import DataTable from "@/components/Data-table";
+
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-full min-h-screen p-4 bg-gray-950 text-gray-100 overflow-x-hidden">
+      <main className="container mx-auto">
+        <DataTable />
+      </main>
+    </div>
+  );
 };
 
 export default App;
