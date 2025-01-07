00755e977be5443c99b28ea786015090ba30a864
saiful7778 <saiful.islam.rafi.88@gmail.com>
2025-01-08 00:34:51 +0600

    Add: reusable input component

---
+++ src/components/utilities/Input.tsx
@@ -0,0 +1,20 @@
+import cn from "@/lib/utils/cn";
+import { forwardRef } from "react";
+
+const Input = forwardRef<
+  HTMLInputElement,
+  React.InputHTMLAttributes<HTMLInputElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <input
+      className={cn(
+        "bg-gray-950 text-sm py-1 px-2 rounded-md border border-gray-700",
+        className
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+
+export { Input };
